8acd2930f312f3c2bcd0811241dcb2b4ed19d463
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-09-15 22:36:21 +0200

    add delete topic cmd

--- cmd/kaf/topic.go
+++ cmd/kaf/topic.go
@@ -11,10 +11,11 @@ import (
 func init() {
 	rootCmd.AddCommand(topicCmd)
 	topicCmd.AddCommand(createTopicCmd)
+	topicCmd.AddCommand(deleteTopicCmd)
 
-	createTopicCmd.Flags().StringP("partitions", "p", "1", "Number of partitions")
+	createTopicCmd.Flags().Int32P("partitions", "p", int32(1), "Number of partitions")
 	viper.BindPFlag("partitions", createTopicCmd.Flags().Lookup("partitions"))
-	createTopicCmd.Flags().StringP("replicas", "r", "1", "Number of replicas")
+	createTopicCmd.Flags().Int16P("replicas", "r", int16(1), "Number of replicas")
 	viper.BindPFlag("replicas", createTopicCmd.Flags().Lookup("replicas"))
 
 }
@@ -25,7 +26,7 @@ var topicCmd = &cobra.Command{
 }
 
 var createTopicCmd = &cobra.Command{
-	Use:   "create",
+	Use:   "create TOPIC",
 	Short: "Create a topic",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
@@ -41,7 +42,26 @@ var createTopicCmd = &cobra.Command{
 		if err != nil {
 			fmt.Printf("Could not create topic %v: %v\n", args[0], err.Error())
 		} else {
-			fmt.Printf("Created topic %v\n.", args[0])
+			fmt.Printf("Created topic %v.\n", args[0])
+		}
+	},
+}
+
+var deleteTopicCmd = &cobra.Command{
+	Use:   "delete TOPIC",
+	Short: "Delete a topic",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		admin, err := getClusterAdmin()
+		if err != nil {
+			panic(err)
+		}
+
+		err = admin.DeleteTopic(args[0])
+		if err != nil {
+			fmt.Printf("Could not delete topic %v: %v\n", args[0], err.Error())
+		} else {
+			fmt.Printf("Deleted topic %v.\n", args[0])
 		}
 	},
 }
