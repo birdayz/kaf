a852c85eb7202df983c527839bafc2e483a7bc1d
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-09-17 11:48:58 +0200

    add produce

---
+++ cmd/kaf/produce.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/Shopify/sarama"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	rootCmd.AddCommand(produceCmd)
+
+	produceCmd.Flags().StringP("key", "k", "", "Key for the record. Currently only strings are supported.")
+	viper.BindPFlag("key", produceCmd.Flags().Lookup("key"))
+
+	produceCmd.Flags().IntP("num", "n", 1, "Number of records to send.")
+	viper.BindPFlag("num", produceCmd.Flags().Lookup("num"))
+
+}
+
+var produceCmd = &cobra.Command{
+	Use:   "produce TOPIC",
+	Short: "Produce record. Reads data from stdin.",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		config := sarama.NewConfig()
+		config.Version = sarama.V1_0_0_0
+		config.Producer.Return.Successes = true
+
+		producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, config)
+		if err != nil {
+			panic(err)
+		}
+
+		data, err := ioutil.ReadAll(os.Stdin)
+
+		for i := 0; i < viper.GetInt("num"); i++ {
+			partition, offset, err := producer.SendMessage(&sarama.ProducerMessage{
+				Topic: args[0],
+				Key:   sarama.StringEncoder(viper.GetString("key")),
+				Value: sarama.ByteEncoder(data),
+			})
+			if err != nil {
+				fmt.Printf("Failed to send record: %v.", err)
+				os.Exit(1)
+			}
+
+			fmt.Printf("Sent record to partition %v at offset %v.\n", partition, offset)
+
+		}
+
+	},
+}
