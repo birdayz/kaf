f2fc0468b84d3a3da53b5d9ec89790c6c2fe6f75
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-09-19 23:03:21 +0200

    remove obsolete comments

--- cmd/kaf/kaf.go
+++ cmd/kaf/kaf.go
@@ -61,14 +61,7 @@ func main() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kafka/config)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func extractValue(key, input string) (unquoted string, ok bool) {
