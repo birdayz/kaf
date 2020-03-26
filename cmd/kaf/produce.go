79255647d635f0d7ba0cd1fa20d048f2a835bf34
Johannes Brüderl <johannes.bruederl@gmail.com>
2020-03-26 23:42:17 +0100

    produce: add timestamp and proto-key support

--- cmd/kaf/produce.go
+++ cmd/kaf/produce.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"os"
 
+	"time"
+
 	"github.com/Shopify/sarama"
 	"github.com/burdiyan/kafkautil"
 	pb "github.com/golang/protobuf/proto"
@@ -14,6 +16,7 @@ import (
 var keyFlag string
 var numFlag int
 var partitionerFlag string
+var timestampFlag string
 
 func init() {
 	rootCmd.AddCommand(produceCmd)
@@ -24,7 +27,10 @@ func init() {
 	produceCmd.Flags().StringSliceVar(&protoFiles, "proto-include", []string{}, "Path to proto files")
 	produceCmd.Flags().StringSliceVar(&protoExclude, "proto-exclude", []string{}, "Proto exclusions (path prefixes)")
 	produceCmd.Flags().StringVar(&protoType, "proto-type", "", "Fully qualified name of the proto message type. Example: com.test.SampleMessage")
+
+	produceCmd.Flags().StringVar(&keyProtoType, "key-proto-type", "", "Fully qualified name of the proto key type. Example: com.test.SampleMessage")
 	produceCmd.Flags().StringVar(&partitionerFlag, "partitioner", "", "Select partitioner: Default or jvm")
+	produceCmd.Flags().StringVar(&timestampFlag, "timestamp", "", "Select timestamp for record")
 
 }
 
@@ -48,6 +54,9 @@ var produceCmd = &cobra.Command{
 			errorExit("Unable to read data\n")
 		}
 
+		var key sarama.Encoder
+		key = sarama.StringEncoder(keyFlag)
+
 		if protoType != "" {
 			if dynamicMessage := reg.MessageForType(protoType); dynamicMessage != nil {
 				err = dynamicMessage.UnmarshalJSON(data)
@@ -64,11 +73,36 @@ var produceCmd = &cobra.Command{
 			}
 		}
 
+		if keyProtoType != "" {
+			if dynamicMessage := reg.MessageForType(keyProtoType); dynamicMessage != nil {
+				err = dynamicMessage.UnmarshalJSON([]byte(keyFlag))
+				if err != nil {
+					errorExit("Failed to parse input JSON as proto type %v: %v", protoType, err)
+				}
+
+				pb, err := pb.Marshal(dynamicMessage)
+				if err != nil {
+					errorExit("Failed to marshal proto: %v", err)
+				}
+
+				key = sarama.ByteEncoder(pb)
+			}
+		}
+
+		var ts time.Time
+		t, err := time.Parse(time.RFC3339, timestampFlag)
+		if err != nil {
+			ts = time.Now()
+		} else {
+			ts = t
+		}
+
 		for i := 0; i < numFlag; i++ {
 			partition, offset, err := producer.SendMessage(&sarama.ProducerMessage{
-				Topic: args[0],
-				Key:   sarama.StringEncoder(keyFlag),
-				Value: sarama.ByteEncoder(data),
+				Topic:     args[0],
+				Key:       key,
+				Timestamp: ts,
+				Value:     sarama.ByteEncoder(data),
 			})
 			if err != nil {
 				fmt.Printf("Failed to send record: %v.", err)
