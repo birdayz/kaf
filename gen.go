f0368295e7a55a5c96a5dd26864aff953ef23634
Johannes Brüderl <johannes.bruederl@gmail.com>
2020-04-13 22:22:08 +0200

    move gen.go to /

--- gen.go
+++ gen.go
@@ -6,18 +6,19 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/shurcooL/vfsgen"
 	"os"
 	"time"
+
+	"github.com/shurcooL/vfsgen"
 )
 
 //go:generate bash -c "npm i && npm run build -- --spa"
 //go:generate bash -c "go run gen.go"
 
 func main() {
-	var fs http.FileSystem = http.Dir("./dist/")
+	var fs http.FileSystem = http.Dir("./client/dist/")
 
-	err := vfsgen.Generate(modTimeFS{fs:fs}, vfsgen.Options{
+	err := vfsgen.Generate(modTimeFS{fs: fs}, vfsgen.Options{
 		PackageName:  "client",
 		VariableName: "Assets",
 	})
@@ -28,36 +29,37 @@ func main() {
 
 // modTimeFS is an http.FileSystem wrapper that modifies
 // underlying fs such that all of its file mod times are set to zero.
+// We do this to not have a diff every time we re-generate files, even if they are the same.
 type modTimeFS struct {
-    fs http.FileSystem
+	fs http.FileSystem
 }
 
 func (fs modTimeFS) Open(name string) (http.File, error) {
-    f, err := fs.fs.Open(name)
-    if err != nil {
-        return nil, err
-    }
-    return modTimeFile{f}, nil
+	f, err := fs.fs.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return modTimeFile{f}, nil
 }
 
 type modTimeFile struct {
-    http.File
+	http.File
 }
 
 func (f modTimeFile) Stat() (os.FileInfo, error) {
-    fi, err := f.File.Stat()
-    if err != nil {
-        return nil, err
-    }
-    return modTimeFileInfo{fi}, nil
+	fi, err := f.File.Stat()
+	if err != nil {
+		return nil, err
+	}
+	return modTimeFileInfo{fi}, nil
 }
 
 type modTimeFileInfo struct {
-    os.FileInfo
+	os.FileInfo
 }
 
 func (modTimeFileInfo) ModTime() time.Time {
-    return time.Time{}
-    // or any custom logic you'd like to implement for your needs
-    // (perhaps use the modtime of the protobuf files you're generating from)
+	return time.Time{}
+	// or any custom logic you'd like to implement for your needs
+	// (perhaps use the modtime of the protobuf files you're generating from)
 }
