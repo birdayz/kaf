46864ba028a8b1035dd0b2cbbff05e1346f95fb3
libo.huang <huanglibo2010@gmail.com>
2020-12-04 15:14:03 +0800

    produce byline

--- cmd/kaf/produce.go
+++ cmd/kaf/produce.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -17,10 +19,13 @@ import (
 var (
 	keyFlag         string
 	headerFlag      []string
-	numFlag         int
+	repeatFlag      int
 	partitionerFlag string
 	timestampFlag   string
 	partitionFlag   int32
+	bufferSizeFlag  int
+	byLineFlag      bool
+	inputModeFlag   string
 )
 
 func init() {
@@ -28,17 +33,44 @@ func init() {
 
 	produceCmd.Flags().StringVarP(&keyFlag, "key", "k", "", "Key for the record. Currently only strings are supported.")
 	produceCmd.Flags().StringArrayVarP(&headerFlag, "header", "H", []string{}, "Header in format <key>:<value>. May be used multiple times to add more headers.")
-	produceCmd.Flags().IntVarP(&numFlag, "num", "n", 1, "Number of records to send.")
+	produceCmd.Flags().IntVarP(&repeatFlag, "repeat", "n", 1, "Repeat records to send.")
 
 	produceCmd.Flags().StringSliceVar(&protoFiles, "proto-include", []string{}, "Path to proto files")
 	produceCmd.Flags().StringSliceVar(&protoExclude, "proto-exclude", []string{}, "Proto exclusions (path prefixes)")
 	produceCmd.Flags().StringVar(&protoType, "proto-type", "", "Fully qualified name of the proto message type. Example: com.test.SampleMessage")
 
 	produceCmd.Flags().StringVar(&keyProtoType, "key-proto-type", "", "Fully qualified name of the proto key type. Example: com.test.SampleMessage")
-	produceCmd.Flags().StringVar(&partitionerFlag, "partitioner", "", "Select partitioner: Default or jvm")
+	produceCmd.Flags().StringVar(&partitionerFlag, "partitioner", "", "Select partitioner: [jvm|rand|rr|hash]")
 	produceCmd.Flags().StringVar(&timestampFlag, "timestamp", "", "Select timestamp for record")
 	produceCmd.Flags().Int32VarP(&partitionFlag, "partition", "p", -1, "Partition to produce to")
 
+	produceCmd.Flags().IntVarP(&bufferSizeFlag, "buffer-size", "B", 0, "Buffer size for scanning input")
+	produceCmd.Flags().StringVarP(&inputModeFlag, "input-mode", "M", "single", "Scanning input mode: [single|byline]")
+}
+
+func readLines(reader io.Reader, out chan []byte) {
+	scanner := bufio.NewScanner(os.Stdin)
+	if bufferSizeFlag > 0 {
+		scanner.Buffer(make([]byte, bufferSizeFlag), bufferSizeFlag)
+	}
+
+	for scanner.Scan() {
+		out <- scanner.Bytes()
+	}
+	close(out)
+
+	if err := scanner.Err(); err != nil {
+		errorExit("scanning input failed: %v\n", err)
+	}
+}
+
+func readSingle(reader io.Reader, out chan []byte) {
+	data, err := ioutil.ReadAll(inReader)
+	if err != nil {
+		errorExit("Unable to read data\n")
+	}
+	out <- data
+	close(out)
 }
 
 var produceCmd = &cobra.Command{
@@ -49,8 +81,13 @@ var produceCmd = &cobra.Command{
 	PreRun:            setupProtoDescriptorRegistry,
 	Run: func(cmd *cobra.Command, args []string) {
 		cfg := getConfig()
-		if partitionerFlag != "" {
+		switch partitionerFlag {
+		case "jvm":
 			cfg.Producer.Partitioner = kafkautil.NewJVMCompatiblePartitioner
+		case "rand":
+			cfg.Producer.Partitioner = sarama.NewRandomPartitioner
+		case "rr":
+			cfg.Producer.Partitioner = sarama.NewRoundRobinPartitioner
 		}
 
 		if partitionFlag != int32(-1) {
@@ -62,32 +99,16 @@ var produceCmd = &cobra.Command{
 			errorExit("Unable to create new sync producer: %v\n", err)
 		}
 
-		data, err := ioutil.ReadAll(inReader)
-		if err != nil {
-			errorExit("Unable to read data\n")
+		out := make(chan []byte, 1)
+		switch inputModeFlag {
+		case "single":
+			go readSingle(inReader, out)
+		default:
+			go readLines(inReader, out)
 		}
 
 		var key sarama.Encoder
 		key = sarama.StringEncoder(keyFlag)
-
-		if protoType != "" {
-			if dynamicMessage := reg.MessageForType(protoType); dynamicMessage != nil {
-				err = dynamicMessage.UnmarshalJSON(data)
-				if err != nil {
-					errorExit("Failed to parse input JSON as proto type %v: %v", protoType, err)
-				}
-
-				pb, err := pb.Marshal(dynamicMessage)
-				if err != nil {
-					errorExit("Failed to marshal proto: %v", err)
-				}
-
-				data = pb
-			} else {
-				errorExit("Failed to load payload proto type")
-			}
-		}
-
 		if keyProtoType != "" {
 			if dynamicMessage := reg.MessageForType(keyProtoType); dynamicMessage != nil {
 				err = dynamicMessage.UnmarshalJSON([]byte(keyFlag))
@@ -107,14 +128,6 @@ var produceCmd = &cobra.Command{
 
 		}
 
-		var ts time.Time
-		t, err := time.Parse(time.RFC3339, timestampFlag)
-		if err != nil {
-			ts = time.Now()
-		} else {
-			ts = t
-		}
-
 		var headers []sarama.RecordHeader
 		for _, h := range headerFlag {
 			v := strings.SplitN(h, ":", 2)
@@ -126,24 +139,52 @@ var produceCmd = &cobra.Command{
 			}
 		}
 
-		for i := 0; i < numFlag; i++ {
-			msg := &sarama.ProducerMessage{
-				Topic:     args[0],
-				Key:       key,
-				Headers:   headers,
-				Timestamp: ts,
-				Value:     sarama.ByteEncoder(data),
-			}
-			if partitionFlag != -1 {
-				msg.Partition = partitionFlag
-			}
-			partition, offset, err := producer.SendMessage(msg)
-			if err != nil {
-				fmt.Fprintf(outWriter, "Failed to send record: %v.", err)
-				os.Exit(1)
+		for data := range out {
+			if protoType != "" {
+				if dynamicMessage := reg.MessageForType(protoType); dynamicMessage != nil {
+					err = dynamicMessage.UnmarshalJSON(data)
+					if err != nil {
+						errorExit("Failed to parse input JSON as proto type %v: %v", protoType, err)
+					}
+
+					pb, err := pb.Marshal(dynamicMessage)
+					if err != nil {
+						errorExit("Failed to marshal proto: %v", err)
+					}
+
+					data = pb
+				} else {
+					errorExit("Failed to load payload proto type")
+				}
 			}
 
-			fmt.Fprintf(outWriter, "Sent record to partition %v at offset %v.\n", partition, offset)
+			var ts time.Time
+			t, err := time.Parse(time.RFC3339, timestampFlag)
+			if err != nil {
+				ts = time.Now()
+			} else {
+				ts = t
+			}
+
+			for i := 0; i < repeatFlag; i++ {
+				msg := &sarama.ProducerMessage{
+					Topic:     args[0],
+					Key:       key,
+					Headers:   headers,
+					Timestamp: ts,
+					Value:     sarama.ByteEncoder(data),
+				}
+				if partitionFlag != -1 {
+					msg.Partition = partitionFlag
+				}
+				partition, offset, err := producer.SendMessage(msg)
+				if err != nil {
+					fmt.Fprintf(outWriter, "Failed to send record: %v.", err)
+					os.Exit(1)
+				}
+
+				fmt.Fprintf(outWriter, "Sent record to partition %v at offset %v.\n", partition, offset)
+			}
 		}
 
 	},
