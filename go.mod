5704df122664915852b114a5ec84f876fe10bbd9
Johannes Brüderl <johannes.bruederl@gmail.com>
2021-08-05 19:49:19 +0200

    remove vendor, upgrade to go 1.16

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/birdayz/kaf
 
-go 1.13
+go 1.16
 
 require (
 	github.com/Landoop/schema-registry v0.0.0-20190327143759-50a5701c1891
--- vendor/github.com/lovoo/goka/codec.go
+++
@@ -1,7 +0,0 @@
-package goka
-
-// Codec decodes and encodes from and to []byte
-type Codec interface {
-	Encode(value interface{}) (data []byte, err error)
-	Decode(data []byte) (value interface{}, err error)
-}
